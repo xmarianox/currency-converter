8da52ed6ac54b5ca7f77840b340e94ce3c89f01d
Mariano Molina <xsin.crueldadx@gmail.com>
2016-09-12 21:23:10 -0300

    Initial Commit

---
+++ currency-converter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  currency-converter
+//
+//  Created by Mariano Molina on 12/9/16.
+//  Copyright © 2016 marianomolina.xyz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
