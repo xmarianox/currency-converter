5022b5505ba5c2b13606958212a3e70e84933de6
Mariano Molina <xsin.crueldadx@gmail.com>
2016-09-14 03:05:05 -0300

    Add vista de resultado

--- currency-converter/ChartViewController.swift
+++ currency-converter/ChartViewController.swift
@@ -10,30 +10,29 @@ import UIKit
 
 class ChartViewController: UIViewController {
     
-    
+    // MARK: Outlets
     @IBOutlet weak var resultAmount: UILabel!
-    
     @IBOutlet weak var euroRowRate: UILabel!
     @IBOutlet weak var euroRowLabel: UILabel!
-
     @IBOutlet weak var realRowRate: UILabel!
     @IBOutlet weak var realRowLabel: UILabel!
-    
     @IBOutlet weak var yenRowRate: UILabel!
     @IBOutlet weak var yenRowLabel: UILabel!
-    
     @IBOutlet weak var poundRowRate: UILabel!
     @IBOutlet weak var poundRowLabel: UILabel!
     
+    
+    // MARK: Vars
     var rates = [Rate]()
     var amountSegue: String?
     
+    
+    // MARK: Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
         if let currentAmount = amountSegue {
             resultAmount.text = "$\(currentAmount)"
-            
             getLastRate(currentAmount)
         }
     }
@@ -49,8 +48,10 @@ class ChartViewController: UIViewController {
     }
     
     
+    // MARK: Methods
+    // Get data from API.
     func getLastRate(amount: String) {
-        // Get data from API.
+        
         let endPoint = EndPoint()
         
         let services = CurrencyServices()
@@ -63,7 +64,7 @@ class ChartViewController: UIViewController {
         }
     }
     
-    
+    // Add data to views
     func setData(rates: [Rate], amount: String) {
         // set euro data
         euroRowRate.text = calculateAmount(amount, currentRate: rates[3].rateValue)
@@ -82,6 +83,7 @@ class ChartViewController: UIViewController {
         realRowLabel.text = rates[0].rateName
     }
     
+    // Calculate Rate
     func calculateAmount(userAmount: String, currentRate: Double) -> String {
         let result = Double(userAmount)! * currentRate
         
